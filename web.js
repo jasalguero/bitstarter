959aab25e6d5504d6c0f383e6d5c0cfad7b8b98a
jasalguero <jose.salguero@groupon.com>
2013-07-18 22:02:38 +0200

    added responsive design and styling

--- web.js
+++ web.js
@@ -4,14 +4,13 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
   fs.readFile('index.html', function(err,data){
     response.send(data.toString());
   });
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
