c5423fb32530dc73661368c5a68dc1200df5d3d4
jasalguero <jose.salguero@groupon.com>
2013-07-18 23:12:16 +0200

    added new styles

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var fs = require('fs')
+var path = require('path');
+
 
 var app = express.createServer(express.logger());
+
+//static files
+app.use('/css', express.static(path.join(__dirname,'/css')))
+app.use('/imgs', express.static(path.join(__dirname,'/imgs')))
 
 app.get('/', function(request, response) {
   fs.readFile('index.html', function(err,data){
